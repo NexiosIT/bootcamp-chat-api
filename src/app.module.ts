3b131cfae1300cf1eeb3aca026610e07586b23a5
Jasper Vercnocke <jve@nexiosit.com>
2022-12-05 06:25:57 +0100

    load monodb_url from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppGateway } from './app.gateway';
 import { AuthModule } from './auth/auth.module';
@@ -9,14 +10,19 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SocketModule,
     AuthModule,
     UsersModule,
     ChatroomModule,
     MessageModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://api:Hepfod-canvyr-1dykvu@cluster0.ltvdow2.mongodb.net/dev?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
   ],
   providers: [AppGateway],
 })
